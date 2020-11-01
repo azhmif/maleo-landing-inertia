4cb52e170efa8be35eaf46368e2122d2a15e5077
Fauzal azhmi <dev.azhmif@gmail.com>
2020-11-01 09:45:29 +0700

    migration, factory and seeder generate

---
+++ database/factories/KontakFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\kontak;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class kontakFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = kontak::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
